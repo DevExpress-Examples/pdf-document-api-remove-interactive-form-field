eb39e4ba7e0cd362280df880c9a9bf90f20b17ed
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RemoveInteractiveForm/Program.cs
@@ -0,0 +1,19 @@
+﻿using DevExpress.Pdf;
+
+namespace RemoveInteractiveForm {
+    class Program {
+        static void Main(string[] args) {
+            using (PdfDocumentProcessor processor = new PdfDocumentProcessor()) {
+
+                // Load a document with interactive form.
+                processor.LoadDocument("..\\..\\InteractiveForm.pdf");
+
+                // Remove a form field by its name.
+                processor.RemoveFormField("FirstName");
+
+                // Save the result document.
+                processor.SaveDocument("..\\..\\Result.pdf");
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to delete a particular form field from a PDF document 
+
+
+This example shows how to remove a particular form field from a PDF document knowing its name.
+
+<br/>
+
+
