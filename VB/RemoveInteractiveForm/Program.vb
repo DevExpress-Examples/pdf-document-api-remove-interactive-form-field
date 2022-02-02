059ff6358012c38bce6863484d63ab1cd106c445
DevExpressExampleBot <fakeEmail>
2022-02-02 15:26:33 +0300

    Source auto update [skip ci]

--- VB/RemoveInteractiveForm/Program.vb
+++ VB/RemoveInteractiveForm/Program.vb
@@ -1,16 +1,15 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 
 Namespace RemoveInteractiveForm
-    Friend Class Program
-        Shared Sub Main(ByVal args() As String)
-            Using processor As New PdfDocumentProcessor()
 
+    Friend Class Program
+
+        Shared Sub Main(ByVal args As String())
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document with interactive form.
                 processor.LoadDocument("..\..\InteractiveForm.pdf")
-
                 ' Remove a form field by its name.
                 processor.RemoveFormField("FirstName")
-
                 ' Save the result document.
                 processor.SaveDocument("..\..\Result.pdf")
             End Using
